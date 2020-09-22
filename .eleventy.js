9d5d97e5348923ece48bed540610a8eb95030e72
Viktor Demydov <vicd129@gmail.com>
2020-09-22 22:38:55 +0300

    migrate data

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,9 @@
 module.exports = {
     htmlTemplateEngine: "njk"
 };
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("svg");
+    eleventyConfig.addPassthroughCopy("img");
+  }
