a7f39c20601694b76a02e1ee581477e4a411f2a3
Viktor Demydov <vicd129@gmail.com>
2020-10-14 15:02:14 +0300

    titles fix

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,17 @@
 module.exports = {
-    htmlTemplateEngine: "njk"
+  htmlTemplateEngine: "njk"
 };
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy({
-        "./node_modules/bootstrap/dist/css/bootstrap.min.css": "./css/bootstrap.min.css",
-      });
-    eleventyConfig.addPassthroughCopy("css");
-    eleventyConfig.addPassthroughCopy("svg");
-    eleventyConfig.addPassthroughCopy("img");
-    eleventyConfig.addPassthroughCopy({
-        "./node_modules/bootstrap/dist/js/bootstrap.min.js": "./js/bootstrap.min.js",
-        "./node_modules/popper.js/dist/umd/popper.min.js": "./js/popper.min.js",
-        "./node_modules/dark-mode-toggle/dist/dark-mode-toggle.min.mjs": "./js/dark-mode-toggle.min.mjs",
-      });
-  }
+  eleventyConfig.addPassthroughCopy({
+    "./node_modules/bootstrap/dist/css/bootstrap.min.css": "./css/bootstrap.min.css",
+  });
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("svg");
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy({
+    "./node_modules/bootstrap/dist/js/bootstrap.min.js": "./js/bootstrap.min.js",
+    "./node_modules/popper.js/dist/umd/popper.min.js": "./js/popper.min.js",
+    "./node_modules/dark-mode-toggle/dist/dark-mode-toggle.min.mjs": "./js/dark-mode-toggle.min.mjs",
+  });
+}
