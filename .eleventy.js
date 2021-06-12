ec21a8395ae784d832de566b10c9a1dd4fa00a75
Viktor Demydov <vicd129@gmail.com>
2021-06-12 10:54:30 +0300

    update 2 06.2021

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,5 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     "./node_modules/bootstrap/dist/js/bootstrap.min.js": "./js/bootstrap.min.js",
     "./node_modules/popper.js/dist/umd/popper.min.js": "./js/popper.min.js",
-    "./node_modules/dark-mode-toggle/dist/dark-mode-toggle.min.mjs": "./js/dark-mode-toggle.min.mjs",
   });
 }
